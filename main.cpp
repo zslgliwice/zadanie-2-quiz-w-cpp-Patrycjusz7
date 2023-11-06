7e7b4530fa6f2ca43d04d280ec76edd59c952003
Patrycjusz7 <144687472+Patrycjusz7@users.noreply.github.com>
2023-11-06 18:21:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,49 +6,77 @@ using namespace std;
 
 string imie;
 string odp;
+int punkt = 0;
 
-int main()
+vaid wyswietl()
 {
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
+    cout<<"Podaj imie: ";
+    cin>>imie;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
 
-    system("cls");
-    cout<< "3";
+    cout<<"3";
+    Sleep(1000);
+    cout<<"3..2";
     Sleep(1000);
     system("cls");
-    cout<< "3..2";
+    cout<<"3..2..1";
     Sleep(1000);
     system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
+    cout<<imie<<"witaj w quizie\n";
+}
 
-    cout << imie << " witaj w tescie z programowania\n";
 
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
+void Pytanie(const string& tresc, const string& odpPopr, const string& opcjaA, const string& opcjaB, const string& opcjaC) {
+    cout << tresc << endl;
+    cout << "a) " << opcjaA << endl;
+    cout << "b) " << opcjaB << endl;
+    cout << "c) " << opcjaC << endl;
+
+    cout << "Podaj odpowiedź: ";
     cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
-    }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
 
+    if (odp == odpPopr) {
+        cout << "Poprawna odpowiedź!\n";
+        pkt++;
+    } else {
+        cout << "Zła odpowiedź:(\n";
     }
-    Sleep(3000);
+}
+
+int main() {
+    wyswietl();
+
+    Pytanie("Pytanie pierwsze: ile kół ma rower?", "a", "2", "100", "4");
+
     system("cls");
 
+    wyswietl();
 
+    Pytanie("Pytanie drugie: Pumba z filmu Król Lew był?:", "b", "lwem", "guzcem", "malpą");
+
+    system("cls");
+
+    wyswietl();
+
+    Pytanie("Pytanie trzecie: Jaki jest najmniejszy ptak na świecie?:", "c", "sęp", "kon", "koliber pszczol");
+
+    system("cls");
+
+    wyswietl();
+
+    Pytanie("Pytanie czwarte: Jaka jest stolica Portugalii?:", "c", "warszawa", "berlin", "lizbona");
+
+    system("cls");
+
+    cout << "Twoja liczba poprawnych odpowiedzi: " << pkt << "/4\n";
+
+    if (pkt <= 1) {
+        cout << "Słabo Ci poszło.\n";
+    } else if (pkt >= 2 && pkt <= 3) {
+        cout << "Nie jest źle.\n";
+    } else if (pkt == 4) {
+        cout << "Dobrze Ci poszło!\n";
+    }
 
     return 0;
 }
